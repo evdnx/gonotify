6c7897874ade93d290c27ee29b8ffafaa20ff9ef
ed <github.k50o8@passmail.net>
2026-01-09 15:27:02 +1100

    add SendFile function

--- messenger/telegram/telegram.go
+++ messenger/telegram/telegram.go
@@ -4,16 +4,20 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
+	"mime/multipart"
 	"net/http"
+	"os"
+	"path/filepath"
 	"time"
 )
 
 // Client is a client for sending messages to Telegram
 type Client struct {
-	botToken  string
-	chatID    string
+	botToken   string
+	chatID     string
 	httpClient *http.Client
-	apiURL    string
+	apiURL     string
 }
 
 // Message represents a message to be sent to Telegram
@@ -32,7 +36,7 @@ type Response struct {
 func NewClient(botToken, chatID string) *Client {
 	return &Client{
 		botToken: botToken,
-		chatID:  chatID,
+		chatID:   chatID,
 		httpClient: &http.Client{
 			Timeout: 10 * time.Second,
 		},
@@ -85,8 +89,71 @@ func (c *Client) SendMessage(message string) error {
 	return nil
 }
 
+// SendFile sends a file to the Telegram chat using sendDocument API
+func (c *Client) SendFile(filePath string) error {
+	// Open the file
+	file, err := os.Open(filePath)
+	if err != nil {
+		return fmt.Errorf("failed to open file: %w", err)
+	}
+	defer file.Close()
+
+	// Create multipart form data
+	var requestBody bytes.Buffer
+	writer := multipart.NewWriter(&requestBody)
+
+	// Add chat_id field
+	err = writer.WriteField("chat_id", c.chatID)
+	if err != nil {
+		return fmt.Errorf("failed to write chat_id field: %w", err)
+	}
+
+	// Add document field
+	filename := filepath.Base(filePath)
+	part, err := writer.CreateFormFile("document", filename)
+	if err != nil {
+		return fmt.Errorf("failed to create form file: %w", err)
+	}
+
+	// Copy file content to the form
+	_, err = io.Copy(part, file)
+	if err != nil {
+		return fmt.Errorf("failed to copy file content: %w", err)
+	}
+
+	// Close the writer to finalize the multipart message
+	err = writer.Close()
+	if err != nil {
+		return fmt.Errorf("failed to close writer: %w", err)
+	}
+
+	// Create the HTTP request
+	url := fmt.Sprintf("%s/bot%s/sendDocument", c.apiURL, c.botToken)
+	req, err := http.NewRequest("POST", url, &requestBody)
+	if err != nil {
+		return fmt.Errorf("failed to create HTTP request: %w", err)
+	}
+
+	// Set content type
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+
+	// Send the request
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to send HTTP request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	// Check response
+	if resp.StatusCode != http.StatusOK {
+		responseBody, _ := io.ReadAll(resp.Body)
+		return fmt.Errorf("telegram API error: status %d, body: %s", resp.StatusCode, string(responseBody))
+	}
+
+	return nil
+}
+
 // Name returns the name of the messenger
 func (c *Client) Name() string {
 	return "Telegram"
 }
-
